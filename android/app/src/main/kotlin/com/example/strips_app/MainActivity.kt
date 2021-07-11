7a2692a6674aa021c0d17c6b84a448e991115bf1
XxExecut0rxX <82061636+XxExecut0rxX@users.noreply.github.com>
2021-07-11 16:41:54 -0400

    🚒 first commit adding flutter strips app added .gitignore

---
+++ android/app/src/main/kotlin/com/example/strips_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.strips_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/description_place.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DescriptionPlace extends StatelessWidget {
+  const DescriptionPlace({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
